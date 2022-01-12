756de81204e3ae10dda2d9c857bacf4899802496
Adrien GOGOIS <ad.gogois@gmail.com>
2022-01-12 15:29:06 +0100

    Add pilotRover Function and display in the grid

--- rover.js
+++ rover.js
@@ -12,8 +12,6 @@ const grid = [
   [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
 ];
 
-console.table(grid);
-
 let rover = {
   direction: "N",
   x: 0,
@@ -24,18 +22,22 @@ function turnLeft(rover) {
   switch (rover.direction) {
     case "N":
       rover.direction = "W";
+      grid[rover.x][rover.y] = rover.direction;
       break;
 
     case "W":
       rover.direction = "S";
+      grid[rover.x][rover.y] = rover.direction;
       break;
 
     case "S":
       rover.direction = "E";
+      grid[rover.x][rover.y] = rover.direction;
       break;
 
     case "E":
       rover.direction = "N";
+      grid[rover.x][rover.y] = rover.direction;
       break;
   }
 }
@@ -44,18 +46,22 @@ function turnRight(rover) {
   switch (rover.direction) {
     case "N":
       rover.direction = "E";
+      grid[rover.x][rover.y] = rover.direction;
       break;
 
     case "E":
       rover.direction = "S";
+      grid[rover.x][rover.y] = rover.direction;
       break;
 
     case "S":
       rover.direction = "W";
+      grid[rover.x][rover.y] = rover.direction;
       break;
 
     case "W":
       rover.direction = "N";
+      grid[rover.x][rover.y] = rover.direction;
       break;
   }
 }
@@ -64,18 +70,45 @@ function moveForward(rover) {
   switch (rover.direction) {
     case "N":
       rover.y++;
+      grid[rover.x][rover.y] = rover.direction;
       break;
 
     case "E":
       rover.x++;
+      grid[rover.x][rover.y] = rover.direction;
       break;
 
     case "S":
       rover.y--;
+      grid[rover.x][rover.y] = rover.direction;
       break;
 
     case "W":
       rover.x--;
+      grid[rover.x][rover.y] = rover.direction;
       break;
   }
 }
+
+function pilotRover(commands) {
+  if (commands === undefined) {
+    return console.log("Enter a valid command !");
+  } else {
+    for (let i = 0; i < commands.length; i++) {
+      let command = commands[i];
+
+      if (command === "l") {
+        turnLeft(rover);
+      } else if (command === "r") {
+        turnRight(rover);
+      } else if (command === "f") {
+        moveForward(rover);
+      }
+    }
+  }
+}
+
+turnRight(rover);
+moveForward(rover);
+
+console.table(grid);
